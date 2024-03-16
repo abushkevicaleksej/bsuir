82524d99b7bc8f8b868e9223e8bd90c5e0c8a7c1
Alexei_Abushkevich <112658913+abushkevicaleksej@users.noreply.github.com>
2024-03-16 23:57:37 +0300

    repushed FloatNumber

--- AOIS/AOIS_LW1/FloatNumber.h
+++ AOIS/AOIS_LW1/FloatNumber.h
@@ -1,69 +1,49 @@
-#pragma once
 #include <iostream>
-class FloatNumber
-{
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class FloatNumber {
 private:
-    
-    const std::string clearMantiss = "00000000000000000000000"; 
-    const std::string exp127 = "01111111"; 
-    const std::string clearExp = "00000000";
-    const std::string binOne = "00000001";
-    const std::string binMinusOne = "11111111";
-    
-    std::string minus;
-    std::string exp;
-    std::string mantiss;
-
-    void SeparateParts(float decNum, std::string& intPart, std::string& behindPoinPart);
-
-    void MakeIntBinary(int decNum, std::string& output);
-
-    void MakeBehindPointBinary(float DecNum, std::string& output);
-
-    std::string BinarySum(std::string binaryNum1, std::string binaryNum2);
-
-    bool IsExpMore(std::string exp1, std::string exp2);
-
-    void MoveNum(std::string& mantiss, int added);
-
-    void MantiissEqualize(FloatNumber& num1, FloatNumber& num2);
-
-    FloatNumber MantissSum(FloatNumber& num1, FloatNumber& num2);
-
-    std::string MakeAdditional(std::string binaryNum);
-
-    std::string BinNumInverse(std::string binaryNum);
-
-    float IntPartInFloat(std::string intPart);
-
-    float BehindPointPartInFloat(std::string behindPointPart);
-
-    void FillByNumWithIntPart(std::string& intPart, std::string& behindPointPart, int& index);
-
-    void FillByNumWhithNoIntPart(std::string& intPart, std::string& behindPointPart, int& index);
-
-    void SumOfTwoOnes(std::string& num1, std::string& num2, bool& oneForNextDigit, int index);
-
-    void SumOfOneAndZero(std::string& num1, std::string& num2, bool& oneForNextDigit, int index);
-
-    void SumOfTwoZeros(std::string& num1, std::string& num2, bool& oneForNextDigit, int index);
-
-    void ConvertToStandartFormat(std::string& num1MantissBuff, FloatNumber& num1, FloatNumber& output);
-
-    void MultByExpWithNegativeDegree(FloatNumber& copyOfMainNum, std::string& intPart, std::string& behindPointPart);
-
-    void MultByExpWithPositiveDegree(FloatNumber& copyOfMainNum, std::string& intPart, std::string& behindPointPart);
+    string sign;
+    string exponent;
+    string mantissa;
 
 public:
     FloatNumber();
+    FloatNumber(double num);
+    void Convert(double num);
+    string GetBinaryRepresentation() const;
+    void ConsoleOutput();
+    string FloatToBinary(double num) const;
+    FloatNumber operator+(const FloatNumber& other) const;
 
-    FloatNumber(float input_num);
+private:
+    string IntToBinary(int num) const;
+    string PadWithZeros(const string& input, int targetLength) const;
+    double BinaryToFloat() const;
+};
 
-    FloatNumber operator+(const FloatNumber& num1);
 
-    void operator=(const FloatNumber& num1);
+FloatNumber::FloatNumber() : sign("0"), exponent(""), mantissa("") {}
 
-    std::string printFloatNumber();
+FloatNumber::FloatNumber(double num) : sign("0"), exponent(""), mantissa("") {
+    Convert(num);
+}
 
-    float calcInDec();
-}; 
+string PadWithZeros(const string& input, int targetLength);
+
+string FloatToBinary(double num);
+
+void Convert(double num);
+
+string GetBinaryRepresentation();
+
+void ConsoleOutput();
+
+string IntToBinary(int num);
+
+FloatNumber operator+(const FloatNumber& other);
+
+double BinaryToFloat();
