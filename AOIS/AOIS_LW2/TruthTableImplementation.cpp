4a11706e269c88c4088804a01bbbe5c03bcc508c
Alexei_Abushkevich <112658913+abushkevicaleksej@users.noreply.github.com>
2024-03-23 13:04:18 +0300

    Update TruthTableImplementation.cpp

--- AOIS/AOIS_LW2/TruthTableImplementation.cpp
+++ AOIS/AOIS_LW2/TruthTableImplementation.cpp
@@ -1,4 +1,4 @@
-﻿#include "TruthTable.h"
+#include "TruthTable.h"
 
 int IsOperator(char c)
 {
@@ -28,9 +28,9 @@ int IsUpperAlpha(char c)
 {
     return isupper(c);
 }
-int Priority(char opr)
+int Priority(char operation)
 {
-    switch (opr)
+    switch (operation)
     {
     case '!':
         return 5;
@@ -44,267 +44,197 @@ int Priority(char opr)
         return 0;
     }
 }
-int IsValidExpression(const string& exp)
+
+int IsValidExpression(const string& expression)
 {
-    if (exp.empty())
+    if (expression.empty())
     {
-        cout << "You didn't enter anything!" << endl;
-        return 0;
+        throw std::invalid_argument("You didn't enter anything!");
     }
 
     string::const_iterator pos;
-    pos = find_if(exp.begin(), exp.end(), IsInvalidChar);
-    if (pos != exp.end())
+    pos = find_if(expression.begin(), expression.end(), IsInvalidChar);
+    if (pos != expression.end())
     {
-        cout << "Invalid characters: '" << *pos << '\'' << endl;
-        return 0;
+        throw std::invalid_argument("Invalid characters: '" + string(1, *pos) + "'");
     }
-
-    if (!isupper(*(exp.begin())) && *(exp.begin()) != '(' && *(exp.begin()) != '!')
-    {
-        cout << "The beginning character is wrong!" << endl;
-        return 0;
-    }
-    if (!isupper(*(exp.rbegin())) && *(exp.rbegin()) != ')')
-    {
-        cout << "The end character is wrong!" << endl;
-        return 0;
-    }
-
-    if (0 == count_if(exp.begin(), exp.end(), IsUpperAlpha))
-    {
-        cout << "No meta!" << endl;
-        return 0;
-    }
-
-    if (count(exp.begin(), exp.end(), '(') != count(exp.begin(), exp.end(), ')'))
-    {
-        cout << "The number of parentheses varies!" << endl;
-        return 0;
-    }
-
     vector<int> bracketpos[2];
-    for (pos = exp.begin(); pos != exp.end(); ++pos)
+    for (pos = expression.begin(); pos != expression.end(); ++pos)
     {
         if (*pos == '(')
-            bracketpos[0].push_back(distance(exp.begin(), pos));
+            bracketpos[0].push_back(distance(expression.begin(), pos));
         else if (*pos == ')')
-            bracketpos[1].push_back(distance(exp.begin(), pos));
+            bracketpos[1].push_back(distance(expression.begin(), pos));
     }
-    vector<int>::size_type v;
-    for (v = 0; v != bracketpos[0].size(); ++v)
+    vector<int>::size_type vector;
+    for (vector = 0; vector != bracketpos[0].size(); ++vector)
     {
-        if (bracketpos[0][v] > bracketpos[1][v])
+        if (bracketpos[0][vector] > bracketpos[1][vector])
         {
-            cout << "Bracket positions don't match!" << endl;
-            return 0;
-        }
-    }
-
-    string::size_type i;
-    for (i = 0; i != exp.size() - 1; ++i)
-    {
-        if (isupper(exp[i]))
-        {
-            if (exp[i + 1] != ')' && !IsBinaryOperator(exp[i + 1]))
-            {
-                cout << '\'' << exp[i] << "'There's something wrong on the right! mistake:" << exp[i] << exp[i + 1] << endl;
-                return 0;
-            }
-        }
-        else if (exp[i] == '(')
-        {
-            if (exp[i + 1] == ')' || IsBinaryOperator(exp[i + 1]))
-            {
-                cout << '\'' << exp[i] << "'There's something wrong on the right! mistake:" << exp[i] << exp[i + 1] << endl;
-                return 0;
-            }
-        }
-        else if (exp[i] == ')')
-        {
-            if (exp[i + 1] != ')' && !IsBinaryOperator(exp[i + 1]))
-            {
-                cout << '\'' << exp[i] << "'There's something wrong on the right! mistake:" << exp[i] << exp[i + 1] << endl;
-                return 0;
-            }
-        }
-        else if (exp[i] == '!')
-        {
-            if (exp[i + 1] == ')' || IsBinaryOperator(exp[i + 1]))
-            {
-                cout << '\'' << exp[i] << "'There's something wrong on the right! mistake:" << exp[i] << exp[i + 1] << endl;
-                return 0;
-            }
-        }
-        else
-        {
-            if (exp[i + 1] == ')' || IsBinaryOperator(exp[i + 1]))
-            {
-                cout << '\'' << exp[i] << "'There's something wrong on the right! mistake:" << exp[i] << exp[i + 1] << endl;
-                return 0;
-            }
+            throw std::invalid_argument("Bracket positions don't match!");
         }
     }
     return 1;
 }
 
-set<char> GetExpInfo(const string& exp)
-{
-    set<char> exp_elem;
 
-    for (string::size_type i = 0; i != exp.size(); ++i)
+set<char> GetExpressionInfo(const string& expression)
+{
+    set<char> expression_elem;
+
+    for (string::size_type i = 0; i != expression.size(); ++i)
     {
-        if (isupper(exp[i]))
+        if (isupper(expression[i]))
         {
-            exp_elem.insert(exp[i]);
+            expression_elem.insert(expression[i]);
         }
     }
-    return exp_elem;
+    return expression_elem;
 }
 
-string InfixToSuffix(const string& exp)
+string InfixToSuffix(const string& expression)
 {
     char top;
-    stack<char> s;
+    stack<char> stack;
     string suffix;
 
-    for (string::size_type i = 0; i != exp.size(); ++i)
+    for (string::size_type i = 0; i != expression.size(); ++i)
     {
-        if (isupper(exp[i]))
-            suffix += exp[i];
-        else if (exp[i] == '(')
-            s.push(exp[i]);
-        else if (IsOperator(exp[i]))
+        if (isupper(expression[i]))
+            suffix += expression[i];
+        else if (expression[i] == '(')
+            stack.push(expression[i]);
+        else if (IsOperator(expression[i]))
         {
             while (1)
             {
-                if (s.empty() || s.top() == '(')
+                if (stack.empty() || stack.top() == '(')
                 {
-                    s.push(exp[i]);
+                    stack.push(expression[i]);
                     break;
                 }
                 else
                 {
-                    top = s.top();
-                    if (Priority(exp[i]) > Priority(top) || (exp[i] == '!' && top == '!'))
+                    top = stack.top();
+                    if (Priority(expression[i]) > Priority(top) || (expression[i] == '!' && top == '!'))
                     {
-                        s.push(exp[i]);
+                        stack.push(expression[i]);
                         break;
                     }
                     else
                     {
-                        suffix += s.top();
-                        s.pop();
+                        suffix += stack.top();
+                        stack.pop();
                     }
                 }
             }
         }
-        else if (exp[i] == ')')
+        else if (expression[i] == ')')
         {
-            while (!s.empty() && (top = s.top()) != '(')
+            while (!stack.empty() && (top = stack.top()) != '(')
             {
                 suffix += top;
-                s.pop();
+                stack.pop();
             }
-            s.pop();
+            stack.pop();
         }
 
     }
-    while (!s.empty())
+    while (!stack.empty())
     {
-        suffix += s.top();
-        s.pop();
+        suffix += stack.top();
+        stack.pop();
     }
     return suffix;
 }
 
-int Eval(const string& row, const string& suffix, const set<char>& exp_elem)
+int Eval(const string& row, const string& suffix, const set<char>& expression_elem)
 {
-    char temp = 0;
-    int p, q, ret;
-    stack<char> s;
-    map<char, int> m;
+    char intermediate = 0;
+    int first, second, result;
+    stack<char> stack;
+    map<char, int> map;
     string::size_type i;
 
     set<char>::const_iterator pos;
-    for (pos = exp_elem.begin(), i = 0; pos != exp_elem.end(); ++pos, ++i)
+    for (pos = expression_elem.begin(), i = 0; pos != expression_elem.end(); ++pos, ++i)
     {
-        m.insert(pair<char, int>(*pos, row[i] - '0'));
+        map.insert(pair<char, int>(*pos, row[i] - '0'));
     }
 
-    p = q = 0;
+    first = second = 0;
     for (i = 0; i != suffix.size(); ++i)
     {
         if (isupper(suffix[i]))
-            s.push(m[suffix[i]]);
+            stack.push(map[suffix[i]]);
         else if (IsOperator(suffix[i]))
         {
-            q = s.top();
-            s.pop();
+            second = stack.top();
+            stack.pop();
 
-            if (suffix[i] != '!' && !s.empty())
+            if (suffix[i] != '!' && !stack.empty())
             {
-                p = s.top();
-                s.pop();
+                first = stack.top();
+                stack.pop();
             }
 
             switch (suffix[i])
             {
             case '!':
-                temp = !q;
+                intermediate = !second;
                 break;
             case '&':
-                temp = p && q;
+                intermediate = first && second;
                 break;
             case '|':
-                temp = p || q;
+                intermediate = first || second;
                 break;
             case '>':
-                temp = !p || q;
+                intermediate = !first || second;
                 break;
             }
-            s.push(temp);
+            stack.push(intermediate);
         }
     }
-    ret = s.top();
-    s.pop();
-    return ret;
+    result = stack.top();
+    stack.pop();
+    return result;
 }
 
-vector<char> PrintTable(const string& exp, vector<int>& index)
+vector<char> PrintTable(const string& expression, vector<int>& index)
 {
-    int cols, rows, temp;
+    int cols, rows, intermediate;
     string row, suffix;
-    set<char> exp_elem;
+    set<char> expression_elem;
 
-    exp_elem = GetExpInfo(exp);
-    for (set<char>::iterator pos = exp_elem.begin(); pos != exp_elem.end(); ++pos)
+    expression_elem = GetExpressionInfo(expression);
+    for (set<char>::iterator pos = expression_elem.begin(); pos != expression_elem.end(); ++pos)
     {
         cout << '\t' << *pos;
     }
-    cout << '\t' << exp << endl;
+    cout << '\t' << expression << endl;
 
-    suffix = InfixToSuffix(exp);
-    cols = exp_elem.size();
+    suffix = InfixToSuffix(expression);
+    cols = expression_elem.size();
     rows = static_cast<int>(pow(2.0, cols));
 
-    bitset<26> bs;
+    bitset<26> bsize;
     vector<char> result;
     for (int i = 0; i < rows; ++i)
     {
-        bs = i;
-        row = bs.to_string();
-        row = bs.to_string<char, char_traits<char>, allocator<char> >();
+        bsize = i;
+        row = bsize.to_string();
+        row = bsize.to_string<char, char_traits<char>, allocator<char> >();
         row.erase(0, 26 - cols);
         for (int j = 0; j < cols; ++j)
         {
             cout << '\t' << row[j];
         }
-        temp = Eval(row, suffix, exp_elem);
-        result.push_back(temp);
-        cout << '\t' << temp << endl;
+        intermediate = Eval(row, suffix, expression_elem);
+        result.push_back(intermediate);
+        cout << '\t' << intermediate << endl;
 
-        index.push_back(temp);
+        index.push_back(intermediate);
     }
     return result;
 }
@@ -332,125 +262,105 @@ int IsContradiction(const vector<char>& result)
 }
 
 
-void PrintCnf(const vector<char>& result, const set<char>& exp_elem)
+void PrintCnf(const vector<char>& result, const set<char>& expression_elem)
 {
     if (IsTautology(result))
         return;
 
-    vector<char> elem;
-    copy(exp_elem.begin(), exp_elem.end(), back_inserter(elem));
+    vector<char> element;
+    copy(expression_elem.begin(), expression_elem.end(), back_inserter(element));
 
     size_t i, j;
-    vector<int> v;
+    vector<int> vector;
     for (i = 0; i != result.size(); ++i)
     {
         if (result[i] == 0)
-            v.push_back(i);
+            vector.push_back(i);
     }
 
     cout << "CNF\n" << "CNF Index Form (";
-    for (i = 0; i != v.size(); ++i)
+    for (i = 0; i != vector.size(); ++i)
     {
-        if (i < v.size() - 1)
-            cout << v[i] << ", ";
+        if (i < vector.size() - 1)
+            cout << vector[i] << ", ";
         else
-            cout << v[i];
+            cout << vector[i];
     }
     cout << ')' << endl;
 
-    bitset<26> bs;
+    bitset<26> bsize;
     string row;
-    for (i = 0; i != v.size(); ++i)
+    for (i = 0; i != vector.size(); ++i)
     {
         cout << '(';
-        bs = v[i];
-        row = bs.to_string();
-        row = bs.to_string<char, char_traits<char>, allocator<char> >();
-        row.erase(0, 26 - elem.size());
-        for (j = 0; j != elem.size(); ++j)
+        bsize = vector[i];
+        row = bsize.to_string();
+        row = bsize.to_string<char, char_traits<char>, allocator<char> >();
+        row.erase(0, 26 - element.size());
+        for (j = 0; j != element.size(); ++j)
         {
             if (row[j] == '1')
-                cout << "!" << elem[j];
+                cout << "!" << element[j];
             else
-                cout << elem[j];
+                cout << element[j];
 
-            if (j < elem.size() - 1)
+            if (j < element.size() - 1)
                 cout << " | ";
         }
-        if (i < v.size() - 1)
+        if (i < vector.size() - 1)
             cout << ") & ";
         else
             cout << ')' << endl;
     }
 }
 
-void PrintDnf(const vector<char>& result, const set<char>& exp_elem)
+void PrintDnf(const vector<char>& result, const set<char>& expression_elem)
 {
     if (IsContradiction(result))
         return;
 
-    vector<char> elem;
-    copy(exp_elem.begin(), exp_elem.end(), back_inserter(elem));
+    vector<char> element;
+    copy(expression_elem.begin(), expression_elem.end(), back_inserter(element));
 
     size_t i, j;
-    vector<int> v;
+    vector<int> vector;
     for (i = 0; i != result.size(); ++i)
     {
         if (result[i] == 1)
-            v.push_back(i);
+            vector.push_back(i);
     }
 
     cout << "DNF\n" << "DNF Index Form (";
-    for (i = 0; i != v.size(); ++i)
+    for (i = 0; i != vector.size(); ++i)
     {
-        if (i < v.size() - 1)
-            cout << v[i] << ", ";
+        if (i < vector.size() - 1)
+            cout << vector[i] << ", ";
         else
-            cout << v[i];
+            cout << vector[i];
     }
     cout << ')' << endl;
 
-    bitset<26> bs;
+    bitset<26> bsize;
     string row;
-    for (i = 0; i != v.size(); ++i)
+    for (i = 0; i != vector.size(); ++i)
     {
         cout << '(';
-        bs = v[i];
-        row = bs.to_string<char, char_traits<char>, allocator<char> >();
-        row.erase(0, 26 - elem.size());
-        for (j = 0; j != elem.size(); ++j)
+        bsize = vector[i];
+        row = bsize.to_string<char, char_traits<char>, allocator<char> >();
+        row.erase(0, 26 - element.size());
+        for (j = 0; j != element.size(); ++j)
         {
             if (row[j] == '1')
-                cout << elem[j];
+                cout << element[j];
             else
-                cout << "!" << elem[j];
+                cout << "!" << element[j];
 
-            if (j < elem.size() - 1)
+            if (j < element.size() - 1)
                 cout << " & ";
         }
-        if (i < v.size() - 1)
+        if (i < vector.size() - 1)
             cout << ") | ";
         else
             cout << ')' << endl;
     }
-}
-
-int main()
-{
-    cout << "Input expr:" << endl;
-    //string expr = "(A|B)&(!C)";
-    vector<string>parsed_expr;
-
-    string expr;
-    cin >> expr;
-    vector<int>index;
-    set<char> exp_elem;
-    vector<char> result;
-    double decimal = 0;
-    exp_elem = GetExpInfo(expr);
-    result = PrintTable(expr, index);
-    decimal = CountIndexNumber(decimal, index);
-    PrintCnf(result, exp_elem);
-    PrintDnf(result, exp_elem);
-    return 0;
 }
